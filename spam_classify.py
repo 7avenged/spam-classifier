164e368fed702b3654378b6a3bd43e179461bcf8
Aditya Mohan <7avenged@users.noreply.github.com>
2017-10-10 10:49:42 +0530

    Update spam_classify.py

--- spam_classify.py
+++ spam_classify.py
@@ -1,9 +1,9 @@
 import os	#created and improvised by Aditya Mohan #7avenged
 import io
-import numpy  
+import numpy                                 
 from pandas import DataFrame
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.naive_bayes import MultinomialNB
+from sklearn.naive_bayes import MultinomialNB      #THIS SHALL IMPLEMENT NAIVE BAYES
 
 def readFiles(path):
     for root, dirnames, filenames in os.walk(path):
@@ -33,19 +33,21 @@ def dataFrameFromDirectory(path, classification):
     return DataFrame(rows, index=index)
 
 data = DataFrame({'message': [], 'class': []})
-
+#THESE COMMANDS WILL LOAD UP THE DATA(EMAILS OF SPAM AND NON-SPAM IN THE 2 DIFFERENT CLASSES OF THE data VARIABLE,FOR TRAINING
 data = data.append(dataFrameFromDirectory('DIRECTORY CONTAINING THE SPAM DATASET FOLDER/spam', 'A SPAM!!!'))
 data = data.append(dataFrameFromDirectory('DIRECTORY CONTAINING THE NON-SPAM DATASET FOLDER/non-spam', 'Nope!Not a spam !'))
 
-
+#THIS COMMAND WILL TOKENIZE OR CONVERT ALL THE VALUES IN THE COLUMN "MESSAGE" OF THE data VARIABLE 
+#ie. ASSIGN A NUMERICAL VALUE TO EACH INDIVIDUAL WORD IN THE MESSAGE VARIABLE
 vectorizer = CountVectorizer()
-counts = vectorizer.fit_transform(data['message'].values)
+counts = vectorizer.fit_transform(data['message'].values) #SO, IT WILL RECORD HOW MANY TIMES EACH WORD OCCURSIN THE EMAIL
+
 
 classifier = MultinomialNB()
-targets = data['class'].values
-classifier.fit(counts, targets)
+targets = data['class'].values #THIS IS THE CLASSIFICATION DATA FOR EACH EMAIL
+classifier.fit(counts, targets) #MODEL HAS BEN CREATED
 
-
+#NOTE:- NO TEST SET HERE AS I USED SAMPLE TEXT (AFTER CONVERTING IT TO VECTORS  ETC ETC.) FOR TESTING PURPOSE 
 examples = ['Make 5000 Rs. per day!!!, unlock this hidden trick', "Hello Aditya, I am making a team for a hackathon, you interested? Ping me up. "]
 example_counts = vectorizer.transform(examples)
 predictions = classifier.predict(example_counts)
