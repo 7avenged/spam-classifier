8dbad87f6c8499d9d20facd920f46b63637a3c93
Aditya Mohan <7avenged@users.noreply.github.com>
2017-10-25 02:44:48 +0530

    added extended description and comments in teh code

--- spam_classify.py
+++ spam_classify.py
@@ -5,10 +5,10 @@ from pandas import DataFrame
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB      #THIS SHALL IMPLEMENT NAIVE BAYES
 
-def readFiles(path):
-    for root, dirnames, filenames in os.walk(path):
-        for filename in filenames:
-            path = os.path.join(root, filename)
+def readFiles(path):  #this function will move through(iterate) each and every file in the directory
+    for root, dirnames, filenames in os.walk(path):  #os.walk finds all the files in the directory
+        for filename in filenames:              
+            path = os.path.join(root, filename) #this is the full path of each individual file in that directory
 
             inBody = False
             lines = []
@@ -16,7 +16,7 @@ def readFiles(path):
             for line in f:
                 if inBody:
                     lines.append(line)
-                elif line == '\n':
+                elif line == '\n':      #this skips the first line(the header) and moves through the text(usually starting after the first line)
                     inBody = True
             f.close()
             message = '\n'.join(lines)
@@ -32,18 +32,18 @@ def dataFrameFromDirectory(path, classification):
 
     return DataFrame(rows, index=index)
 
-data = DataFrame({'message': [], 'class': []})
+data = DataFrame({'message': [], 'class': []}) #this is an empty variable containing 2 columns named message and class
 #THESE COMMANDS WILL LOAD UP THE DATA(EMAILS OF SPAM AND NON-SPAM IN THE 2 DIFFERENT CLASSES OF THE data VARIABLE,FOR TRAINING
 data = data.append(dataFrameFromDirectory('DIRECTORY CONTAINING THE SPAM DATASET FOLDER/spam', 'A SPAM!!!'))
 data = data.append(dataFrameFromDirectory('DIRECTORY CONTAINING THE NON-SPAM DATASET FOLDER/non-spam', 'Nope!Not a spam !'))
 
 #THIS COMMAND WILL TOKENIZE OR CONVERT ALL THE VALUES IN THE COLUMN "MESSAGE" OF THE data VARIABLE 
 #ie. ASSIGN A NUMERICAL VALUE TO EACH INDIVIDUAL WORD IN THE MESSAGE VARIABLE
-vectorizer = CountVectorizer()
-counts = vectorizer.fit_transform(data['message'].values) #SO, IT WILL RECORD HOW MANY TIMES EACH WORD OCCURSIN THE EMAIL
+vectorizer = CountVectorizer()  #this shall count the frequency of anything that is passed onto this and makes a sparse matrix
+counts = vectorizer.fit_transform(data['message'].values) #SO, HERE IT WILL RECORD HOW MANY TIMES EACH WORD OCCURS IN THE EMAIL. 
 
 
-classifier = MultinomialNB()
+classifier = MultinomialNB()  #here multinomial naive bayes classifier has been has been used
 targets = data['class'].values #THIS IS THE CLASSIFICATION DATA FOR EACH EMAIL
 classifier.fit(counts, targets) #MODEL HAS BEN CREATED
 
@@ -51,5 +51,5 @@ classifier.fit(counts, targets) #MODEL HAS BEN CREATED
 examples = ['Make 5000 Rs. per day!!!, unlock this hidden trick', "Hello Aditya, I am making a team for a hackathon, you interested? Ping me up. "]
 example_counts = vectorizer.transform(examples)
 predictions = classifier.predict(example_counts)
-predictions
+predictions  #the program is run
 
